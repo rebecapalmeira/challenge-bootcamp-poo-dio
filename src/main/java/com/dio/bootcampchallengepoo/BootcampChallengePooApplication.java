6f7ae6bf9415ddbd06d4a063aa1b84e0676c891e
Rebeca Palmeira <rebeca.palmeira@gmail.com>
2022-01-30 19:19:32 -0300

    first commit

---
+++ src/main/java/com/dio/bootcampchallengepoo/BootcampChallengePooApplication.java
@@ -0,0 +1,13 @@
+package com.dio.bootcampchallengepoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampChallengePooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampChallengePooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/bootcampchallengepoo/BootcampChallengePooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.bootcampchallengepoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampChallengePooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
