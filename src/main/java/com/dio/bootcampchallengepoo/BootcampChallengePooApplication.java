7f0f78c13792051977d1456c90f3555809f2ea41
Rebeca Palmeira <rebeca.palmeira@gmail.com>
2022-02-02 08:38:33 -0300

    progress of Devs on bootcamps

--- src/main/java/com/dio/bootcampchallengepoo/BootcampChallengePooApplication.java
+++ src/main/java/com/dio/bootcampchallengepoo/BootcampChallengePooApplication.java
@@ -12,36 +12,45 @@ public class BootcampChallengePooApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootcampChallengePooApplication.class, args);
 
-		Course course1 = new Course();
-		course1.setTitle("Java");
-		course1.setDescription("Curso de Programação Orientada a Objetos em Java");
-		course1.setWorkload(8);
+		Course coursePOOJava = new Course();
+		coursePOOJava.setTitle("Java");
+		coursePOOJava.setDescription("Curso de Programação Orientada a Objetos em Java");
+		coursePOOJava.setWorkload(8);
 
-		Course course2 = new Course();
-		course2.setTitle("Javascript");
-		course2.setDescription("Introdução à Lógica de Programação em Javascript");
-		course2.setWorkload(4);
+		Course courseIntroLogicJava = new Course();
+		courseIntroLogicJava.setTitle("Javascript");
+		courseIntroLogicJava.setDescription("Introdução à Lógica de Programação em Java");
+		courseIntroLogicJava.setWorkload(4);
 
-		Mentorship mentorship1 = new Mentorship("Mentoria de Java Web", "Criando uma Aplicação RESTful", LocalDate.now());
+		Mentorship mentorshipJavaWeb = new Mentorship("Mentoria de Java Web", "Criando uma Aplicação RESTful", LocalDate.now());
 
-//		System.out.println(course1);
-//		System.out.println(course2);
-//		System.out.println(mentorship1);
 
-		Bootcamp bootcamp1 = new Bootcamp();
-		bootcamp1.setName("Bootcamp Java Developer");
-		bootcamp1.setDescription("Descrição Bootcamp Java Developer");
-		bootcamp1.getSubjects().add(course1);
-		bootcamp1.getSubjects().add(course2);
-		bootcamp1.getSubjects().add(mentorship1);
+		Bootcamp bootcampJavaGFT= new Bootcamp();
+		bootcampJavaGFT.setName("Bootcamp Java Developer GFT Estágio");
+		bootcampJavaGFT.setDescription("Bootcamp Java Dev - seleção para programa de estágio GFT");
+		bootcampJavaGFT.getSubjects().add(coursePOOJava);
+		bootcampJavaGFT.getSubjects().add(courseIntroLogicJava);
+		bootcampJavaGFT.getSubjects().add(mentorshipJavaWeb);
 
 		Dev devRebeca = new Dev();
 		devRebeca.setName("Rebeca Palmeira");
-		devRebeca.subscribeToBootcamp(bootcamp1);
+		devRebeca.subscribeToBootcamp(bootcampJavaGFT);
+		System.out.println(devRebeca.getName() + " inscrito(a) nos conteúdos: " + devRebeca.getSubscribedSubjects());
+		devRebeca.progress();
+		devRebeca.progress();
+		devRebeca.progress();
+		System.out.println(devRebeca.getName() + " inscrito(a) nos conteúdos: " + devRebeca.getSubscribedSubjects());
+		System.out.println(devRebeca.getName() + " concluiu os conteúdos: " + devRebeca.getFinishedSubjects());
+		System.out.println(devRebeca.getName() + " XP: " + devRebeca.calculateTotalXp());
 
 		Dev devSara = new Dev();
 		devSara.setName("Sara Almeida");
-		devSara.subscribeToBootcamp(bootcamp1);
+		devSara.subscribeToBootcamp(bootcampJavaGFT);
+		System.out.println(devSara.getName() + " inscrito(a) nos conteúdos: " + devSara.getSubscribedSubjects());
+		devSara.progress();
+		System.out.println(devSara.getName() + " inscrito(a) nos conteúdos: " + devRebeca.getSubscribedSubjects());
+		System.out.println(devSara.getName() + " concluiu os conteúdos: " + devRebeca.getFinishedSubjects());
+		System.out.println(devSara.getName() + " XP: " + devSara.calculateTotalXp());
 
 	}
 
